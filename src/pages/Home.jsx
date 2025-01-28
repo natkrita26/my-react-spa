8cf7f582827a191a18157a458cb7b99a0165d184
Natkrita26 <natkrita.son@dome.tu.ac.th~>
2025-01-28 12:02:16 +0700

    undate

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => { //const = ไม่เปลี่ยน  //() enornimous function => = arrow function 
+    return (
+      <div>
+        <h1>ยินดีต้อนรับสู่ SPA ของฉัน</h1>
+        <p>นี่คือหน้าแรก</p>
+      </div>
+    )
+  }
+
+
+  
+  export default Home
